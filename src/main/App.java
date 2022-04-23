db5790780397ea7086b534f2e4856c64da365c18
lucassoni <lucassonit@gmail.com>
2022-04-23 11:48:43 -0300

    Merge dev main

--- src/main/App.java
+++ src/main/App.java
@@ -4,27 +4,28 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        Cadastro cadastro = new Cadastro();  
-        Paciente user = cadastro.lePaciente(sc);
-        while(true){       
+        Cadastro cadastro = new Cadastro();
+        Paciente user = Cadastro.lePaciente(sc);
+        while (true) {
             System.out.printf(
-                    "Bem vindo %s\nPara cadastrar um familiar digite 1\nPara cadastrar um medico digite 2\nPara sair digite 3\n", user.getNome());
+                    "Bem vindo %s\nPara cadastrar um familiar digite 1\nPara cadastrar um medico digite 2\nPara sair digite 3\n",
+                    user.getNome());
 
             int option = sc.nextInt();
             sc.nextLine();
-            
-            switch (option){
+
+            switch (option) {
                 case 1:
-                    Familiar familiar = cadastro.leFamiliar(sc);
+                    Familiar familiar = Cadastro.leFamiliar(sc);
                     familiar.imprimeFamiliar();
                     break;
                 case 2:
-                    Medico medico = cadastro.leMedico(sc);
+                    Medico medico = Cadastro.leMedico(sc);
                     medico.imprimeMedico();
                     break;
                 case 3:
                     return;
-                default: 
+                default:
                     System.out.println("Numero invalido");
             }
         }
